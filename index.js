b5b77816a6fdc3774422efe7618fe79da7ff3e88
Desmond McNamee <desmondmcnamee@gmail.com>
2018-06-17 21:06:26 +0200

    Testing postgres db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+await client.connect();
 
 const PORT = process.env.PORT || 5000
-
 app.get('/', (req, res) => res.send('Welcome to Price Buddy!'))
 app.listen(PORT, () => console.log('Listening on port:', PORT))
