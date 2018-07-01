a5a760a6c9550f3db2313facb168f4f22aecf75e
Max <max_karl@icloud.com>
2018-07-01 21:30:15 +0200

    fix parser currency and price

--- src/parser/src/handlers/shopParser/amazonParser.js
+++ src/parser/src/handlers/shopParser/amazonParser.js
@@ -1,62 +1,55 @@
 
-var request = require('request');
-var cheerio = require('cheerio');
-var crypto = require('crypto');
-var fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const parseDomain = require('parse-domain');
+const currencyFormatter = require('currency-formatter');
 
-var imgID = '#landingImage';
-var titleID = '#productTitle';
-var priceID = '#priceblock_ourprice';
-//one element inside another
+const imgID = 'data-old-hires';
+const titleID = '#productTitle';
+const priceID = '#priceblock_ourprice';
 
-function split_price_and_currency (price_and_currency){
-  //need improvement its not universal
-  var currency = price_and_currency.substring(0, 3);
-  var price = price_and_currency.substring(4);
-  return {
-    currency: currency,
-    price: price,
-  };
+const currency_mapping = {
+  "de" : "EUR",
+  "com.au" : "AUD",
+  "com.br" : "BRL",
+  "cn" : "CNY",
+  "fr" : "EUR",
+  "ca" : "CAD",
+  "in" : "INR",
+  "it" : "EUR",
+  "com.mx" : "MXN",
+  "co.jp" : "JPY",
+  "nl" : "EUR",
+  "es" : "EUR",
+  "com" : "USD"
+}
+
+function get_currency (url){
+  const tld = parseDomain(url).tld;
+  return currency_mapping[tld];
 }
 
 const parse = (url) => {
-  request('https://www.'+url, function (error, response, html) {
-     var parsed_data = { image : "", name : "", amount : "", currency : "", shop: "Amazon", url };
+  const url_string = 'https://www.'+url;
+  const currency = get_currency (url_string);
+  request(url_string, function (error, response, html) {
+     let parsed_data = { image : "", name : "", amount : "", currency : currency, shop: "Amazon", url };
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
 
       $(titleID).filter(function(){
-        var data = $(this);
+        const data = $(this);
         parsed_data.name=data.text().trim();
       })
 
       $(priceID).filter(function(){
-        var data = $(this);
-        var price_and_currency = data.text();
-        parsed_data.currency=split_price_and_currency(price_and_currency).currency;
-        parsed_data.amount=Number(split_price_and_currency(price_and_currency).price.replace(',','.'));
+        const data = $(this);
+        parsed_data.amount=currencyFormatter.unformat(data.text(),{code : currency});
       })
 
       $(imgID).filter(function(){
-        var data = $(this);
-        //still getting something wrong, looks like image file header
-        //we cant get the imge url https://stackoverflow.com/questions/39177793/src-pull-out-info-as-datagif-instead-of-direct-image-link
-        //still not saving the actual picture just some dot
-        //determine file automatically or use function which get it from string
-        //var imagedata=data.attr('src').replace(/^data:image\/gif;base64,/, "");
-        var imagepath=data.attr('data-old-hires');
-        //var imagename=crypto.createHmac('sha256','our_secret_is_the_best').update( parsed_data.name+Math.random().toString()).digest('hex');
-        //determine file extension automatically
-        //var imagepath='parsed_images/'+imagename+'.gif';
-        //fs.writeFile(imagepath, imagedata, 'base64', function(err){
-          //  if (err) throw err
-            //console.log('File saved.');
-        //})
-        /*fs.writeFile('test.html', html, function(err){
-            if (err) throw err
-            console.log('File saved.');
-        })*/
-        parsed_data.image=imagepath;
+        const data = $(this);
+        parsed_data.image=data.attr(imgID);
         })
 
       }
